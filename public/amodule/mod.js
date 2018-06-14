afb0735f16150a17c2b2d35e441e0dc14e80105d
Agrawal <shreya.agrawal@capgemini.com>
2018-06-14 16:09:45 +0530

    promise propert used

--- public/amodule/mod.js
+++ public/amodule/mod.js
@@ -2,21 +2,35 @@ var mod = angular.module('restmod',[]);
 
 mod.controller("rest",['httpservice','$scope',function(httpservice,$scope){
     $scope.pattern="";
-    $scope.companies =[];
+    $scope.companies = [];
     $scope.readpattern = function(){
-     console.log($scope.pattern);
-     httpservice.getCompanies($scope.pattern);
+     //console.log($scope.pattern);
+    httpservice.getCompanies($scope.pattern).then(
+         (data)=>{
+             $scope.companies = data;
+             $scope.$digest();
+         },
+         (error)=>{
+            $scope.companies = [];
+            $scope.$digest();
+         }
+     );
+     //console.log($scope.companies);
     }
 }]);
 
 
 mod.service('httpservice',['$http',function($http){             //to call a rest api
     this.getCompanies = function(pattern){
-        $http.get("http://localhost:4780/mongo-api/cnames/"+pattern).then(
-            (data)=>{console.log(data)},                                            //if successful
-            (error)=>{console.log(error)}                                           //if error
-        );
+        return new Promise(function(resolve,reject){
+            
+            $http.get("http://localhost:4780/mongo-api/cnames/"+pattern).then(
+            (response)=>{resolve(response.data);},                                            //if successful
+            (error)=>{reject([]);}                                           //if error
+            );
 
+        })
+        
     }
     
 }]);
